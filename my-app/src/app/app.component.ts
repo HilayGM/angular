9b0783afde14b162b1c7aa1b54d7a4ca68ce1ef6
hilayGM <hilailibreros@gmail.com>
2025-01-16 17:53:53 -0600

    new 16/1/25

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 
 
 export class AppComponent {
-  listaEstudiantes: any[] = [
-    {nombre: "thomas", estado:"promocionado"},
-    {nombre: "ana", estado:"regular"},
-]}
+    listaEstudiantes: any[] = [
+        { nombre: "thomas", estado: "promocionado" },
+        { nombre: "juan", estado: "regular" },
+        {nombre : "martin", estado: "libre"}
+    ]
+
+    mostrar = false;
+    toogle(): void {
+        this.mostrar = !this.mostrar;
+
+    }
+    
+
+
+}
