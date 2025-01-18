b5aadc3e2742d964ec8560aec4ae1a5738fb7d49
hilayGM <hilailibreros@gmail.com>
2025-01-17 18:42:19 -0600

    complete

--- dados/src/app/app.component.ts
+++ dados/src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dados';
+
+  dadoizquierdo = '../assets/img/dice1.png';
+  dadoDerecho = '../assets/img/dice4.png';
+
+  numero1: number = 0;
+  numero2: number = 0;
+
+  tirarDados(): void {
+    // Cambiar los dados aleatoriamente cada 100ms durante 5 segundos
+    const interval = setInterval(() => {
+      this.numero1 = Math.floor(Math.random() * 6) + 1;
+      this.numero2 = Math.floor(Math.random() * 6) + 1;
+
+      this.dadoizquierdo = `../assets/img/dice${this.numero1}.png`;
+      this.dadoDerecho = `../assets/img/dice${this.numero2}.png`;
+    }, 100);
+
+    // Detener los cambios después de 5 segundos
+    setTimeout(() => {
+      clearInterval(interval);
+
+      // Generar el resultado final
+      this.numero1 = Math.floor(Math.random() * 6) + 1;
+      this.numero2 = Math.floor(Math.random() * 6) + 1;
+
+      this.dadoizquierdo = `../assets/img/dice${this.numero1}.png`;
+      this.dadoDerecho = `../assets/img/dice${this.numero2}.png`;
+    }, 3000);
+  }
+
 }
